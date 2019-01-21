31268da319434ba6a6241d5fcea12441646ca9c9
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-01-21 22:30:09 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+char alphabet;
+scanf("%c",&alphabet);
+if((alphabet=='a')||(alphabet=='e')||(alphabet=='i')||(alphabet=='o')||(alphabet=='u'))
+{
+    printf("vowels");
+    
+}
+else
+{
+    printf("consonants");
+
+}
+getch();
+
+}
