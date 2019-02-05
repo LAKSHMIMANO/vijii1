0457d9d197c725dc429cdcc4003de126238ec388
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-02-05 20:53:46 +0530

    Create word.c

---
+++ word.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int c=0,i;
+  char s[100];
+  printf ("enter the line");
+  scanf("%[^/n]s",s);
+  for(i=0;s[i]!='\0';i++)
+  {
+    if(s[i]==' ')
+    c++;
+  }
+  printf("%d",c+1);
+return 0;
+}
