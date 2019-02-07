2d1dd2acce2f13ff6f780a627aa0d9d84b3a4150
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-02-07 21:25:29 +0530

    Create nom.c

---
+++ nom.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() 
+{
+  int n;
+  
+  printf("enter the number from 1 to 10");
+  scanf("%d",&n);
+  switch(n)
+  {
+    case1:
+    printf("one");
+    break;
+    case2:
+    printf("two");
+    break;
+    case3:
+    printf("three");
+    break;
+    case4:
+    printf("four");
+    break;
+    case5:
+    printf("five");
+    break;
+    case6:
+    printf("six");
+    break;
+    case7:
+    printf("seven");
+    break;
+    case8:
+    printf("eight");
+    break;
+    case9:
+    printf("nine");
+    break;
+    case10:
+    printf("ten");
+    break;
+    default:
+    printf("no such number");
+  }
+  return 0;
+}
