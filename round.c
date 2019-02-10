47501ba08f40e11dc7a29c8ab8aa14b85e0ccbff
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-02-10 20:22:09 +0530

    Create round.c

---
+++ round.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  int u;
+  float n;
+  printf("enter the number");
+  scanf("%f",n);
+  u=(int)(n+0.5);
+  printf("%d",u);
+	return 0;
+}
+
+  
