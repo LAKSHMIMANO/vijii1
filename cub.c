580812a8c0dc43be3b035497f9f3a26d9cba7976
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-02-10 21:54:34 +0530

    Create cub.c

---
+++ cub.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+ {
+	int l,b,h,volume,tsa;
+	scanf("%d %d %d",&l,&b,&h);
+	
+	volume=l*b*h;
+	tsa=2*((l*b)+(b*h)+(h*l));
+	printf("%d",volume);
+	printf("%d",tsa);
+	return 0;
+	
+}
