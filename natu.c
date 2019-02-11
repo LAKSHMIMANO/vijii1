9369b4eee9ddc22efdd58c2527ce7a7eb0d8dd35
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-02-11 08:30:32 +0530

    Create natu.c

---
+++ natu.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) 
+{
+int num,c=0,i;
+scanf("%d",&num);
+ for(i=1;i<=num;i++)
+ {
+ c=c+i;
+    }
+     printf("%d",c);
+
+  
+
+	return 0;
+}
+  
