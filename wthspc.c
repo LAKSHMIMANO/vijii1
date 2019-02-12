853ca5f38e95391cb503ea950279e62df5c6ad8e
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-02-12 20:56:39 +0530

    Create wthspc.c

---
+++ wthspc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+	
+	int main()
+	{
+	    int n,k;
+      
+      printf("enter the number");
+      scanf("%d %d",&n,&k);
+      
+      printf("%d%d",n,k);
+
+	    return 0;
+}
