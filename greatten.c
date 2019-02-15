e7df0ca613d262131ebc45b2a1483e7ab254fdb2
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-02-15 16:24:09 +0530

    Create greatten.c

---
+++ greatten.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a,value;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(a<10)
+    {
+       printf("%d",10);
+    }
+
+    return 0;
+}
