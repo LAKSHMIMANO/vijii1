3b2558085f28d7ec076a6622cb886632699b05a1
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-02-27 06:35:32 +0530

    Create holi.c

---
+++ holi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char a[100];
+  int i;
+	
+	printf("enter the string");
+	scanf("%s",a);
+  if(a[i]=='S'||a[i]=='s')
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("no");
+  }
+
+  return 0;
+}
