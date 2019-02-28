59b4d2f7795cc537c2b1db6f20b20e2fc57d93f8
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-02-28 07:01:58 +0530

    Update execpt1.c

--- execpt1.c
+++ execpt1.c
@@ -12,20 +12,23 @@ int main()
     }
     for(i=0;i<n;i++)
     {
+        count=0;
         for(j=i+1;j<n;j++)
         {
             if(a[i]==a[j])
             {
-                count++;
-                break;
+                a[j]='@'
+               count++;
             }
             
         }
      
-    
     if(count==0)
     {
-        printf("%d",a);
+    if(a[i]!=='@')
+    {
+        printf("%d",a[i]);
+    }
     }
     
 }
