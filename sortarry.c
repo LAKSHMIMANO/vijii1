c3f0442df5b493fab5c49856fad3276825d64d97
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-02-28 12:57:20 +0530

    Update sortarry.c

--- sortarry.c
+++ sortarry.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],i,c=0;
+    int n,b[100],i,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&b[i]);
     }
     for(i=0;i<n;i++)
     {
         for(int j=i+1;j<n;j++)
         {
-            if(a[i]>a[j])
+            if(b[i]>b[j])
             {
                 c++;
             }
