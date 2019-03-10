797f9af46d6e781556588b5540e0c1ff215395ea
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-03-10 19:30:01 +0530

    Create repted.c

---
+++ repted.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main (){
+    int n,rem,a[100],i=0,j,k,count=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        rem=n%10;
+        a[i]=rem;
+        n=n/10;
+        i++;
+    }
+    for(j=0;j<i;j++)
+    {
+        for(k=0;k<i;k++)
+        {
+            if(a[j]==a[k])
+            {
+                count++;
+            }
+        }
+    }
+    if(count>0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
