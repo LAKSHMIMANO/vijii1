0d6f0831307df65e99ac6804e222c366af555533
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-03-10 19:41:45 +0530

    Create bintooct.c

---
+++ bintooct.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    long int n,rem,oct=0,power=1;
+    
+    scanf("%ld",&n);
+    while(n>0)
+    {
+        rem=n%10;
+        oct=oct+rem*power;
+        n=n/10;
+        power=power*2;
+        
+    }
+    printf("%lo",oct);
+    return 0;
+}
