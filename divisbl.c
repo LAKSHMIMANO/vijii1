35f7b2d48a8a8d653b863a6ddd9429219a15dd47
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-03-14 23:35:39 +0530

    Create divisbl.c

---
+++ divisbl.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,flag=0;
+    
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            
+                flag++;
+            }
+        
+    }
+    if(flag>0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
