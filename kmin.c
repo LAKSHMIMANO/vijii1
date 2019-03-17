173cd5395720fd07585a2c9ccf616621ec103518
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-03-17 13:54:19 +0530

    Create kmin.c

---
+++ kmin.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+  int a[100],k,pos,temp=0,min,i,n;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+     scanf("%d",&a[i]);
+     }
+     k=(n+1)-k;
+     while(temp<=k)
+     {
+         min=a[0];
+         for(i=0;i<n;i++)
+         {
+             if(a[i]>min)
+             {
+               min=a[i];
+               pos=i;
+             }
+         }
+         if(temp==k)
+         {
+             printf("%d",min);
+         }
+         else
+         {
+             a[pos]=0;
+         }
+         temp++;
+     }
+
+    return 0;
+}
