2708ef9888ae765fa2f70c63a55720dad946e8e3
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-03-17 13:46:16 +0530

    Create intlong.c

---
+++ intlong.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int m,n,o,sum;
+  scanf("%d",&n);
+  if(n>=-32678&&n<=32677)
+  {
+      printf("INT");
+  }
+  else
+  printf("LONG");
+  
+    
+  return 0;
+}
