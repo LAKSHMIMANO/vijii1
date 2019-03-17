c32807a11df640c03f72d2b683b845283d88fa4f
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-03-17 20:37:08 +0530

    Create roman.c

---
+++ roman.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+char a[100];
+int i,b,c=0,d=0,e=0,f=0;
+
+scanf("%[^\n]s",a);
+for(b=0;a[b]!='\0';b++);
+for(i=0;i<b;i++)
+{
+ if(a[i]=='I')
+ {
+ if(a[i+1]=='V')
+ {
+ c=4;
+ i++;
+ }
+ else if(a[i+1]=='X')
+ {
+ c=9;
+ i++;
+ }
+ else
+ {
+ c=c+1;
+ }}
+ if(a[i]=='V')
+ d=d+5;
+ if(a[i]=='X')
+ e=e+10;
+ }
+ f=c+d+e;
+ printf("%d",f);
+ return 0;
+ }
