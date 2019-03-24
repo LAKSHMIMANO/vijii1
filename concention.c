288ba8dcb120db297de755e1130a971823890f16
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-03-24 17:57:44 +0530

    Create concention.c

---
+++ concention.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main()
+{
+char a[100],b[100],d[100],i,j,l=0,c,k;
+scanf("%s %s",a,b);
+for(i=0;a[i]!='\0';i++);
+for(j=0;b[j]!='\0';j++);
+if(i>j)
+{
+     for(i=0;i<j;i++)
+     {
+         printf("%c",a[i]);
+     }
+       for(i=0;i<j;i++)
+       {
+           printf("%c",b[i]);
+       }
+     
+}
+else if(j>i)
+{
+    
+for(k=0;k<i;k++)
+{
+    printf("%c",a[k]);
+}
+for(k=0;k<i;k++)
+{
+    printf("%c",b[k]);
+}
+}
+else
+{
+    for(k=0;k<i;k++)
+{
+    printf("%c",a[k]);
+}
+for(k=0;k<i;k++)
+{
+    printf("%c",b[k]);
+}
+        
+    }
+
+return 0;
+}
