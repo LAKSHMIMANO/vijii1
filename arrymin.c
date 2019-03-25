793246143ce61b6074d161af80f9a6ac9825c05c
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-03-25 21:38:12 +0530

    Create arrymin.c

---
+++ arrymin.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int  main()
+{
+	int n,a[1000],i,sum=0,j,min=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	if(n==1)
+	{
+	    printf("%d",a[n-1]);
+	}
+	else
+	{
+	for(i=0;i<n;i++)
+	{
+		 sum=sum+a[i];
+		if(min>sum)
+		{
+		    min=sum;
+		}
+	}
+	printf("%d",min);
+	}
+  return 0;
+	}
