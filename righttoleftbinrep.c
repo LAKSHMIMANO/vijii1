ba35ffd6e400c70d5f91f52cbe489da91601ac04
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-03-26 06:59:25 +0530

    Create righttoleftbinrep.c

---
+++ righttoleftbinrep.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n,bin=0,power=1,rem,rem2,i=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        rem=n%2;
+        bin=rem*power+bin;
+        n=n/2;
+        power=power*10;
+    }
+    while(bin>0)
+    {    ++i;
+        rem2=bin%10;
+        if(rem2==1)
+        {
+            printf("%d",i);
+            break;
+        }
+        bin=bin/10;
+    }
+    return 0;
+}
