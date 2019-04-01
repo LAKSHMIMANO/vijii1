979b526fb733636bdf085bd899018824e53a01f2
LAKSHMIMANO <46838681+LAKSHMIMANO@users.noreply.github.com>
2019-04-01 19:01:10 +0530

    Create hun1.3.c

---
+++ hun1.3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    int n,a[100],k=0,c=0,t,b[100],i,j,p=-1;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==i)
+        {
+            b[k]=a[i];
+            k++;
+            c++;
+        }
+    }
+    if(c>0)
+    {
+    for(i=0;i<k;i++)
+    {
+        for(j=i+1;j<k;j++)
+        {
+            if(b[i]>b[j])
+            {
+                t=b[i];
+                b[i]=b[j];
+                b[j]=t;
+            }
+        }
+    }
+    for(i=0;i<k;i++)
+    {
+        printf("%d ",b[i]);
+    }
+    }
+    else
+    {
+        printf("%d ",p);
+    }
+    return 0;
+}
